a5a330afb58e52aed312f9fe9bb1a79151bb2e6d
cedric sambat <cedricsambat9818@gmail.com>
2022-10-05 14:33:46 +0400

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'res', to: 'restaurant#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
